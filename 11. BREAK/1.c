f21befc11bf1af26336b77308c10dbad7ab4ddf1
islomjon17 <turgunovislomjon107@gmail.com>
2024-01-09 09:28:19 +0500

    updateds

--- 11. BREAK/1.c
+++ 11. BREAK/1.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i == 4)
+        {
+            break;
+        }
+        printf("%d\n", i);
+    }
+
+    ////
+
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i == 4)
+        {
+            continue;
+        }
+        printf("%d\n", i);
+    }
+    /////
+    int i = 0;
+
+    while (i < 10)
+    {
+        if (i == 4)
+        {
+            break;
+        }
+        printf("%d\n", i);
+        i++;
+    }
+    ////
+    int i = 0;
+
+    while (i < 10)
+    {
+        if (i == 4)
+        {
+            i++;
+            continue;
+        }
+        printf("%d\n", i);
+        i++;
+    }
 
     return 0;
 }
